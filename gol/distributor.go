f39a4d3a65d9300dbcf7b631e22458d518c40bd4
averyryu <avery.r.engineer@gmail.com>
2023-11-04 00:56:51 +0000

    "Input done!"

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,10 @@
 package gol
 
+import (
+	"fmt"
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -9,20 +14,82 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
-// distributor divides the work between workers and interacts with other goroutines.
-func distributor(p Params, c distributorChannels) {
+func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+	var cells []util.Cell
 
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			if world[y][x] == 255 {
+				cells = append(cells, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+	return cells
+}
+
+func calculateNextState(p Params, world [][]byte) [][]byte {
+	newWorld := make([][]byte, p.ImageHeight)
+	for y := range newWorld {
+		newWorld[y] = make([]byte, p.ImageWidth)
+	}
+
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			sum := (world[(y-1+p.ImageHeight)%p.ImageHeight][(x-1+p.ImageWidth)%p.ImageWidth])/255 + (world[(y-1+p.ImageHeight)%p.ImageHeight][(x+p.ImageWidth)%p.ImageWidth])/255 + (world[(y-1+p.ImageHeight)%p.ImageHeight][(x+1+p.ImageWidth)%p.ImageWidth])/255 +
+				(world[(y+p.ImageHeight)%p.ImageHeight][(x-1+p.ImageWidth)%p.ImageWidth])/255 + (world[(y+p.ImageHeight)%p.ImageHeight][(x+1+p.ImageWidth)%p.ImageWidth])/255 +
+				(world[(y+1+p.ImageHeight)%p.ImageHeight][(x-1+p.ImageWidth)%p.ImageWidth])/255 + (world[(y+1+p.ImageHeight)%p.ImageHeight][(x+p.ImageWidth)%p.ImageWidth])/255 + (world[(y+1+p.ImageHeight)%p.ImageHeight][(x+1+p.ImageWidth)%p.ImageWidth])/255
+			if world[y][x] == 255 {
+				if sum < 2 {
+					newWorld[y][x] = 0
+				} else if sum == 2 || sum == 3 {
+					newWorld[y][x] = 255
+				} else {
+					newWorld[y][x] = 0
+				}
+			} else {
+				if sum == 3 {
+					newWorld[y][x] = 255
+				}
+			}
+		}
+	}
+	return newWorld
+}
+
+func distributor(p Params, c distributorChannels) {
+	c.ioCommand <- ioInput
+	c.ioFilename <- fmt.Sprintf("%dx%d", p.ImageWidth, p.ImageHeight)
+
+	// distributor divides the work between workers and interacts with other goroutines.
 	// TODO: Create a 2D slice to store the world.
 	world := make([][]byte, p.ImageHeight)
 	for y := range world {
 		world[y] = make([]byte, p.ImageWidth)
+		for x := range world[y] {
+			if <-c.ioInput > 0 {
+				world[y][x] = 255
+			} else {
+				world[y][x] = 0
+			}
+		}
 	}
+
+	newWorld := world
 
 	turn := 0
 
 	// TODO: Execute all turns of the Game of Life.
+	for a := 0; a < p.Turns; a++ {
+		newWorld = calculateNextState(p, newWorld)
+		fmt.Printf("Running!")
+	}
 
 	// TODO: Report the final state using FinalTurnCompleteEvent.
+
+	c.events <- FinalTurnComplete{
+		CompletedTurns: p.Turns,
+		Alive:          calculateAliveCells(p, world),
+	}
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
@@ -32,4 +99,5 @@ func distributor(p Params, c distributorChannels) {
 
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
+
 }
